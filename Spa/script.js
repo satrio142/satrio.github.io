59fbaaca64880e94f0ca159cfafc17a0b31d325d
satrio124 <rio710142@gmail.com>
2022-10-02 15:12:14 +0700

    revisi

---
+++ Spa/script.js
@@ -0,0 +1,7 @@
+<div class="content">
+	<div class = "bungkus">
+		<div class="tengah">
+			<div class="tombol">Slide Toggle</div>
+			</div>
+ <p class="isi">The jQuery slideToggle() method toggles between the slideDown() and slideUp() methods. If the elements have been slide down, slideToggle() will slide them up. <br>If the elements have been slid up, slideToggle() will slide them down. $(selector). slideToggle(speed,callback);</p>
+ </div>
